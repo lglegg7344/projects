5034b8c3ddb62f7259edd29efe0c51fafb2bf7ac
Leon Glegg <lglegg7344@sohamcollege.org.uk>
2014-12-15 17:03:11 +0000

    Update house.py

--- minecraft projects/house.py
+++ minecraft projects/house.py
@@ -1,17 +1,17 @@
-import mcpi.minecraft as minecraft
-import time
-import random
-mc = minecraft.Minecraft.create()
+import mcpi.minecraft as minecraft #imports mcpi.minecraft and calls it minecraft
+import time #imports time
+import random #imports random
+mc = minecraft.Minecraft.create() #gives minecraft.Minecraft.create() the name mc
 
-spruce = 1
+spruce = 1 #gets the block with the item ID of 1 and calls it spruce
 
-mc.setBlocks(-30,-5,-30,30,30,30,0)
+mc.setBlocks(-30,-5,-30,30,30,30,0) #clears an area to build a house
 
-mc.setBlocks(1,0,1,5,6,7,5,spruce)
-mc.setBlocks(2,1,2,4,5,6,0)
-mc.setBlocks(3,1,1,3,2,2,0)
+mc.setBlocks(1,0,1,5,6,7,5,spruce) #creates a solid block of spruce planks
+mc.setBlocks(2,1,2,4,5,6,0) #hollows out the block
+mc.setBlocks(3,1,1,3,2,2,0) #creates a doorway
 
-mc.setBlocks(2,6,2,4,6,6,20)
+mc.setBlocks(2,6,2,4,6,6,20) #creates a window
 
 
 

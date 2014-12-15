60237654e54fef436b5b9efaa672c36cefeeb4b9
Leon Glegg <lglegg7344@sohamcollege.org.uk>
2014-12-15 16:53:54 +0000

    Update flowerpath.py

--- minecraft projects/flowerpath.py
+++ minecraft projects/flowerpath.py
@@ -1,14 +1,15 @@
-import mcpi.minecraft as minecraft
-import time
-mc = minecraft.Minecraft.create()
+import mcpi.minecraft as minecraft #imports mcpi.minecraft and calls it minecraft
+import time #imports time
+mc = minecraft.Minecraft.create() #gives minecraft.Minecraft.create() the name mc
 
-while True:
+while True: #creates an infinite loop
     pos = mc.player.getPos()
     x = pos.x
     y = pos.y
     z = pos.z
 
-    Block = 38
+    Block = 38 #gives the item ID number the name Block
 
-    mc.setBlock(x,y,z,Block)
-    time.sleep(0.2)
+    mc.setBlock(x,y,z,Block) #places a block at your coodinates
+    
+    time.sleep(0.2) #pauses for 2/10 of a second

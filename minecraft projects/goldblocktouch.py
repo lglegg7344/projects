e42024bde794a784e6bff4f019d58da303242fd8
Leon Glegg <lglegg7344@sohamcollege.org.uk>
2014-12-15 16:56:31 +0000

    Update goldblocktouch.py

--- minecraft projects/goldblocktouch.py
+++ minecraft projects/goldblocktouch.py
@@ -1,14 +1,14 @@
-import mcpi.minecraft as minecraft
-import time
-mc = minecraft.Minecraft.create()
+import mcpi.minecraft as minecraft #imports mcpi.minecraft and calls it minecraft
+import time #imports time
+mc = minecraft.Minecraft.create() #gives minecraft.Minecraft.create() the name mc
 
-while True:
+while True: #creates an infinite loop
     pos = mc.player.getPos()
     x = pos.x
     y = pos.y
     z = pos.z
 
-    Block = 41
+    Block = 41 #gives 41 (the item ID) the name Block
 
-    mc.setBlock(x,y-1,z,Block)
-    time.sleep(0.2)
+    mc.setBlock(x,y-1,z,Block) # places a block of gold (Block) underneath your feet
+    time.sleep(0.2) #pauses for 2/10 of a second

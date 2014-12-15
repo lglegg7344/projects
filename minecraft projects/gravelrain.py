c09c3ffa3d8aa4d14816a542dffb6c63f1e30cb4
Leon Glegg <lglegg7344@sohamcollege.org.uk>
2014-12-15 16:59:44 +0000

    Update gravelrain.py

--- minecraft projects/gravelrain.py
+++ minecraft projects/gravelrain.py
@@ -1,10 +1,10 @@
-import mcpi.minecraft as minecraft
-import time
-import random
-mc = minecraft.Minecraft.create()
+import mcpi.minecraft as minecraft #imports mcpi.minecraft and calls it minecraft
+import time #imports time
+import random #imports random
+mc = minecraft.Minecraft.create() #gives minecraft.Minecraft.create() the name mc
 
-while True:
-    pos = mc.player.getPos()
+while True: #creates an infinite loop
+    pos = mc.player.getPos() #gets player's position
     x = int(pos.x)
     print x
     y = pos.y
@@ -12,10 +12,10 @@ while True:
     z = int(pos.z)
     print z
 
-    Block = 13
+    Block = 13 #gives 13 (the block ID) the name Block
 
     x = random.randint(x-10,x+10)
     z = random.randint(z-10,z+10)
-    mc.setBlock(x, y+50, z, Block)
-    time.sleep(0.2)
+    mc.setBlock(x, y+50, z, Block) #places a block of gravel above the player
+    time.sleep(0.2) #pauses for 2/10 of a second
                 

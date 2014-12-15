ccddf276558a41b098e6c8eee42b78b974333b72
Leon Glegg <lglegg7344@sohamcollege.org.uk>
2014-12-15 17:53:24 +0000

    Update retirementtime.py

--- etc./retirementtime.py
+++ etc./retirementtime.py
@@ -1,3 +1,3 @@
-age = int(input("what is your age?"))
-retirement = 65 - int(age)
-print("you can retire in {0} years".format (retirement))
+age = int(input("what is your age?")) #asks for your age
+retirement = 65 - int(age) #takes your age away from 65
+print("you can retire in {0} years".format (retirement)) #uses the outcome of the sum and says how many years left until you retire

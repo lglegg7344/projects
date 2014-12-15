866b6e1da65194269c7a4956ae97a5a37b2e2894
Leon Glegg <lglegg7344@sohamcollege.org.uk>
2014-12-15 18:03:26 +0000

    Update and rename leon to leon2.py

--- morse code projects/leon/leon2.py
+++ morse code projects/leon/leon2.py
@@ -1,14 +1,17 @@
-import RPi.GPIO as GP,time
+#outputs Leon in morse code
+
+import RPi.GPIO as GP #imports RPi.GPIO and calls it GP
+import time #imports time
 GP.setmode(GP.BOARD)
 GP.setup(11,GP.OUT)
     
 
-GP.output(11,GP.HIGH)
-time.sleep(0.25)
-GP.ouyput(11,GP.LOW)
+GP.output(11,GP.HIGH) #turns LED on
+time.sleep(0.25) #pauses for .25 of a second
+GP.ouyput(11,GP.LOW) #turns LED off
 time.sleep(025)
 GP.output(11,GP.HIGH)
-time.sleep(1)
+time.sleep(1) #pauses for 1 second
 GP.output(11,GP.LOW)
 time.sleep(0.25)
 GP.output(11,GP.HIGH) 
@@ -47,4 +50,3 @@ time.sleep(0.25)
 GP.output(11,GP.HIGH)
 time.sleep(0.5)
 GP.output(11,GP.LOW)
-

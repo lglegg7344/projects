b51a75762af90d4cee901fe57fbccafb1d8fba21
Leon Glegg <lglegg7344@sohamcollege.org.uk>
2014-12-15 18:05:32 +0000

    Update leon.py

--- morse code projects/leon/leon.py
+++ morse code projects/leon/leon.py
@@ -1,20 +1,26 @@
-import RPi.GPIO as GP,time
+import RPi.GPIO as GP #imports  RPi.GPIO and calls it GP
+import time #imports time
 GP.setmode(GP.BOARD)
 GP.setup(11,GP.OUT)
 
+GP.output(11,GP.HIGH) #turns LED on
+time.sleep(0.25) #pauses for .25s
+GP.output(11,GP.LOW) #turns LED off
+time.sleep(0.5) #pauses for .5s
+GP.output(11,GP.HIGH)
+time.sleep(0.75) #pauses for .75s
+GP.output(11,GP.LOW)
+time.sleep(0.5)
+GP.output(11,GP.HIGH)
+time.sleep(0.25)
+GP.output(11,GP.LOW)
+time.sleep(0.5)
+GP.output(11,GP.HIGH)
+time.sleep(0.25)
+GP.output(11,GP.LOW)
+time.sleep(1) #pauses for 1s
 
-GP.output(11,GP.HIGH)
-time.sleep(0.25)
-GP.output(11,GP.LOW)
-time.sleep(0.5)
-GP.output(11,GP.HIGH)
-time.sleep(0.75)
-GP.output(11,GP.LOW)
-time.sleep(0.5)
-GP.output(11,GP.HIGH)
-time.sleep(0.25)
-GP.output(11,GP.LOW)
-time.sleep(0.5)
+
 GP.output(11,GP.HIGH)
 time.sleep(0.25)
 GP.output(11,GP.LOW)
@@ -22,12 +28,6 @@ time.sleep(1)
 
 
 GP.output(11,GP.HIGH)
-time.sleep(0.25)
-GP.output(11,GP.LOW)
-time.sleep(1)
-
-
-GP.output(11,GP.HIGH)
 time.sleep(0.5)
 GP.output(11,GP.LOW)
 time.sleep(0.75)
@@ -48,5 +48,3 @@ GP.output(11,GP.HIGH)
 time.sleep(0.25)
 GP.output(11,GP.LOW)
 time.sleep(0.75)
-
-

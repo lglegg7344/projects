b9b37eb12a95f6d6474eee9ee4056f9db40feb43
Leon Glegg <lglegg7344@sohamcollege.org.uk>
2014-12-15 17:49:42 +0000

    Update getUserName.py

--- getUser projects/getUserName.py
+++ getUser projects/getUserName.py
@@ -1,12 +1,8 @@
-def getUserName():
+def getUserName(): #defines getUserName()
     check = False
     while check == False:
-        name = input("What is your name?")
-        correct = input ("Confirm that your name is {0} by pressing y, or crrect your name by pressing n".format(name))#you could also say -print("Your name is" + name)-
+        name = input("What is your name?") #asks for user's name
+        correct = input ("Confirm that your name is {0} by pressing y, or crrect your name by pressing n".format(name)) #you could also say -print("Your name is" + name)-
+        #if the user presses y, check is true
         if correct.lower() == "y":
             check = True
-        
-    
-    
-    
-    

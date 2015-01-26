adf94fbce76eec0053093c3bdcb9ca53529b87aa
lglegg7344 <lglegg7344@sohamcollege.org.uk>
2015-01-26 13:33:10 +0000

    balloon

--- balloonpopping.py
+++ balloonpopping.py
@@ -1,14 +1,28 @@
-import picamera,time
+import picamera
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+button = 14
+balloon = 2
+GPIO.setup(button, GPIO.IN, GPIO.PUD_UP)
+GPIO.setup(button, GPIO.OUT)
+print ("Ready...")
+GPIO.wait_for_edge(button, GPIO.FALLING)
+GPIO.output(balloon, True)
+sleep(5)
+GPIO.output(balloon, False)
+print("POP!")
 
 with picamera.PiCamera() as camera:
     camera.resolution = (640,480)
     camera.framerate = 90
-    camera.start_recording('balloon-popping.h264')
+    camera.start_recording('Balloon_Popping.h264')
     camera.start_preview()
     camera.wait_recording(60)
     camera.stop_preview()
     camera.stop_recording()
   
-    
-    
+
+
                            

40282c39611d9aa37a585c8f3d0408099972b9e1
Leon Glegg <lglegg7344@sohamcollege.org.uk>
2015-02-05 15:00:52 +0000

    Update ProgramPlan.py

--- Wiring/ProgramPlan.py
+++ Wiring/ProgramPlan.py
@@ -6,4 +6,24 @@ pin = 17
 count = 0
 
 def spin():
+  global count
+  count +=1
+  print (count)
   
+def calcspeed():
+  radius = 9
+  time = 5
+  CalcSpeed = ((math.pi*radius)*count)/time
+  return CalcSpeed
+  
+def speedkmh():
+  kms = (CalcSpeed/100000)
+  kmh = (kms * 3600)
+  def loop():
+    while True:
+      count = 0 
+      time.sleep(5)
+      spin()
+      calcspeed()
+      speedmph()
+
